b604b430f6cc5ccdfde737861b2892c716dd439c
Anteneh Anley <you@example.com>
2022-08-10 16:08:14 +0000

    do main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Print character */
+char _putchar(char c);
+
+/* Print string */
+int _puts(char *s);
+
+/* Return length of string */
+int _strlen(char *s);
+
+#endif /* MAIN_H */
